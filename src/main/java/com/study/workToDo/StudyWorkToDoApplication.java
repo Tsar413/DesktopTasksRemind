6eba0d3a0f0feed34485c1cc9567b11cf10021de
Tsar413 <xiaolongbao413@163.com>
2026-03-30 22:59:42 +0800

    DataBase Create

---
+++ src/main/java/com/study/workToDo/StudyWorkToDoApplication.java
@@ -0,0 +1,13 @@
+package com.study.workToDo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWorkToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyWorkToDoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/study/workToDo/entity/work.java
@@ -0,0 +1,154 @@
+package com.study.workToDo.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_work")
+@TableName("tb_work")
+public class work {
+    @Id
+    @Column(name = "work_id", length = 100)
+    @TableField("work_id")
+    private String workId;
+
+    @Column(name = "work_title")
+    @TableField("work_title")
+    private String workTitle;
+
+    @Lob
+    @Column(name = "work_content", columnDefinition = "LONGTEXT")
+    @TableField("work_content")
+    private String workContent;
+
+    @Lob
+    @Column(name = "work_comment", columnDefinition = "LONGTEXT")
+    @TableField("work_comment")
+    private String workComment;
+
+    @Column(name = "is_finished")
+    @TableField("is_finished")
+    private Integer isFinished; // 是否完成
+
+    @Column(name = "is_daily_task")
+    @TableField("is_daily_task")
+    private Integer isDailyTask; // 是否是每日任务
+
+    @Column(name = "is_long_life_task")
+    @TableField("is_long_life_task")
+    private Integer isLongLifeTask; // 是否是长时任务；
+
+    @JsonFormat(timezone = "GMT + 8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(columnDefinition = "DATETIME")
+    private LocalDateTime submitTime; // 任务提交时间
+
+    @JsonFormat(timezone = "GMT + 8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(columnDefinition = "DATETIME")
+    private LocalDateTime deadTime; // 任务截止时间
+
+    public work() {
+    }
+
+    public work(String workId, String workTitle, String workContent, String workComment, Integer isFinished, Integer isDailyTask, Integer isLongLifeTask, LocalDateTime submitTime, LocalDateTime deadTime) {
+        this.workId = workId;
+        this.workTitle = workTitle;
+        this.workContent = workContent;
+        this.workComment = workComment;
+        this.isFinished = isFinished;
+        this.isDailyTask = isDailyTask;
+        this.isLongLifeTask = isLongLifeTask;
+        this.submitTime = submitTime;
+        this.deadTime = deadTime;
+    }
+
+    public String getWorkId() {
+        return workId;
+    }
+
+    public void setWorkId(String workId) {
+        this.workId = workId;
+    }
+
+    public String getWorkTitle() {
+        return workTitle;
+    }
+
+    public void setWorkTitle(String workTitle) {
+        this.workTitle = workTitle;
+    }
+
+    public String getWorkContent() {
+        return workContent;
+    }
+
+    public void setWorkContent(String workContent) {
+        this.workContent = workContent;
+    }
+
+    public String getWorkComment() {
+        return workComment;
+    }
+
+    public void setWorkComment(String workComment) {
+        this.workComment = workComment;
+    }
+
+    public Integer getIsFinished() {
+        return isFinished;
+    }
+
+    public void setIsFinished(Integer isFinished) {
+        this.isFinished = isFinished;
+    }
+
+    public Integer getIsDailyTask() {
+        return isDailyTask;
+    }
+
+    public void setIsDailyTask(Integer isDailyTask) {
+        this.isDailyTask = isDailyTask;
+    }
+
+    public Integer getIsLongLifeTask() {
+        return isLongLifeTask;
+    }
+
+    public void setIsLongLifeTask(Integer isLongLifeTask) {
+        this.isLongLifeTask = isLongLifeTask;
+    }
+
+    public LocalDateTime getSubmitTime() {
+        return submitTime;
+    }
+
+    public void setSubmitTime(LocalDateTime submitTime) {
+        this.submitTime = submitTime;
+    }
+
+    public LocalDateTime getDeadTime() {
+        return deadTime;
+    }
+
+    public void setDeadTime(LocalDateTime deadTime) {
+        this.deadTime = deadTime;
+    }
+
+    @Override
+    public String toString() {
+        return "work{" +
+                "workId='" + workId + '\'' +
+                ", workTitle='" + workTitle + '\'' +
+                ", workContent='" + workContent + '\'' +
+                ", workComment='" + workComment + '\'' +
+                ", isFinished=" + isFinished +
+                ", isDailyTask=" + isDailyTask +
+                ", isLongLifeTask=" + isLongLifeTask +
+                ", submitTime=" + submitTime +
+                ", deadTime=" + deadTime +
+                '}';
+    }
+}
